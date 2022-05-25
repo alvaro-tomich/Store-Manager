7c6960361bbdbf518564d927b97fcde6aa65145c
alvaro-tomich <alvaroramos222@hotmail.com>
2022-05-25 16:25:48 -0300

    feat: creating productsModel file and add getProducts

---
+++ models/productsModel.js
@@ -0,0 +1,9 @@
+const connection = require('./connection');
+
+const getProducts = () => {
+    const [products] = connection.execute('SELECT * FROM StoreManager.products');
+
+    return products;
+};
+
+module.exports = { getProducts };
