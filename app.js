202984a3ea2a7e0b5c6b9c83f4681b4907f31912
alvaro-tomich <alvaroramos222@hotmail.com>
2022-05-25 17:06:31 -0300

    feat: connecting the routes in the application

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const route = require('./routes');
 
 const app = express();
+
+app.use(express.json());
+
+app.use(route);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
