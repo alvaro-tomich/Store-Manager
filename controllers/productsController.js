45e045cc5137698e898fb66f527554c7721fd0b9
alvaro-tomich <alvaroramos222@hotmail.com>
2022-05-25 16:40:35 -0300

    feat(controllers/products): creating productsController and GET /products

---
+++ controllers/productsController.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const route = express.Router();
+const productsService = require('../services/productsService');
+
+route.get('/', async (_req, res) => {
+  const product = await productsService.getProducts();
+
+  res.status(200).json(product);
+});
+
+module.exports = route;
