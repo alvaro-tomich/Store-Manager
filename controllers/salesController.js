d27085434a7c065b7fad89ab4ee1436bda6c375c
alvaro-tomich <alvaroramos222@hotmail.com>
2022-05-25 19:49:24 -0300

    feat(controllers): creating sales controller

---
+++ controllers/salesController.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const route = express.Router();
+const salesService = require('../services/productsService');
+
+route.get('/', async (_req, res) => {
+  const [products] = await salesService.getProducts();
+
+  res.status(200).json(products);
+});
+
+route.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const [[product]] = await salesService.getProducts(id);
+  if (!product) return res.status(404).json({ message: 'Product not found' });
+
+  res.status(200).json(product);
+});
+
+module.exports = route;
