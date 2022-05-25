32bc1711e7119e79ead0242d28769ba581ebdfa3
alvaro-tomich <alvaroramos222@hotmail.com>
2022-05-25 19:30:35 -0300

    feat(controllers/products): creating route GET /:id

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -4,7 +4,14 @@ const route = express.Router();
 const productsService = require('../services/productsService');
 
 route.get('/', async (_req, res) => {
-  const [product] = await productsService.getProducts();
+  const [products] = await productsService.getProducts();
+
+  res.status(200).json(products);
+});
+
+route.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const [product] = await productsService.getProducts(id);
 
   res.status(200).json(product);
 });
