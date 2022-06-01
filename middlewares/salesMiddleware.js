bb328b74139eca4acbe1bb73f52a0e6ce015330d
alvaro-tomich <alvaroramos222@hotmail.com>
2022-06-01 17:18:23 -0300

    requirement 3 complete

--- middlewares/salesMiddleware.js
+++ middlewares/salesMiddleware.js
@@ -1,16 +1,31 @@
-const validateSales = (req, res, next) => {
-        req.body.forEach((curr) => {
-        console.log(curr);
-        if (curr.quantity < 1) { 
-        return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' }); 
-        }
-        if (curr.quantity === undefined) {
-            return res.status(400).json({ message: '"quantity" is required' });
-        }
-        if (curr.productId === undefined) {
-            return res.status(400).json({ message: '"productId" is required' });
-        }
+function generateCases(cases) {
+    let quantityMinorCase = 0;
+    let quantityCase = 0;
+    let productCase = 0;
+    cases.forEach((curr) => {
+      if (curr.quantity < 1) {
+        quantityMinorCase += 1;
+      }
+      if (curr.quantity === undefined) {
+        quantityCase += 1;
+      }
+      if (curr.productId === undefined) {
+        productCase += 1;
+      }
     });
+
+    return [quantityMinorCase, quantityCase, productCase];
+}
+
+const validateSales = (req, res, next) => {
+    const cases = generateCases(req.body);
+    if (cases[0] !== 0) {
+        return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
+    } 
+
+    if (cases[1] !== 0) return res.status(400).json({ message: '"quantity" is required' });
+
+    if (cases[2] !== 0) return res.status(400).json({ message: '"productId" is required' });
 
     return next();
 };
